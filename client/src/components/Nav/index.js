33ba6a20bbf37f7f775e06286779753e0f647ff0
Jack Meier <jackson.meier423@gmail.com>
2021-02-12 09:05:29 -0800

    changed navbar bgc to white and removed bootstrap error

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,25 +5,23 @@ import "./style.css";
 
 function NavBar() {
   return (
-    <div className="nav-color">
-      <Navbar collapseOnSelect expand="lg">
-        <Navbar.Brand href="/">My Hikes</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={NavLink} exact to="/">
-              Search
-            </Nav.Link>
-            <Nav.Link as={NavLink} exact to="/completed">
-              Completed Hikes
-            </Nav.Link>
-            <Nav.Link as={NavLink} exact to="/future">
-              Future Hikes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <Navbar collapseOnSelect expand="lg" fixed="top">
+      <Navbar.Brand href="/">My Hikes</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={NavLink} exact to="/">
+            Search
+          </Nav.Link>
+          <Nav.Link as={NavLink} exact to="/completed">
+            Completed Hikes
+          </Nav.Link>
+          <Nav.Link as={NavLink} exact to="/future">
+            Future Hikes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
